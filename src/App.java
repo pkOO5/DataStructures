8714095f24eaa2c7f6974659d40aa4b46727e9d4
pkOO5 <mettupranaykumarreddy@gmail.com>
2023-07-15 18:10:42 -0500

    second version

--- src/App.java
+++ src/App.java
@@ -5,9 +5,15 @@ public class App {
     public static void main(String args[]){
 
         Map<Employee,String> myMap = new HashMap<>();
-        myMap.put(new Employee("Pranay",10),"ABC");
+        Employee employee = new Employee("Pranay",23);
+        Employee employee1 = new Employee("Pranby",23);
 
-
-
+        myMap.put(employee,"ABC");
+        myMap.put(new Employee("Kumar",24),"DEF");
+        myMap.put(new Employee("Karthik",21),"XYZ");
+        System.out.println(myMap);
+       // System.out.println(employee.hashCode() + " " + employee1.hashCode() );
+        myMap.remove(employee.hashCode());
+        System.out.println(myMap);
     }
 }
